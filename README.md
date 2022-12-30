05f524d489423a5a4c9b80bad7cf4d1f13db1ba3
Aubin <aubin.mahe@wanadoo.fr>
2022-12-30 17:55:32 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Extension 'mk'
+
+Ce développement concerne une extension pour Visual Studio Code.
+
+## Fonctions
+
+Cette extension affiche un menu de type *QuickPick* composé des différentes cibles du Makefile du projet courant quand l'utilisateur clique *mk* dans la barre de statut.
---
+++ src/extension.ts
@@ -0,0 +1,66 @@
+import * as vscode from 'vscode';
+
+export function activate( context: vscode.ExtensionContext ) {
+   const sbItem = vscode.window.createStatusBarItem( vscode.StatusBarAlignment.Right, 1000 );
+   sbItem.text    = "mk";
+   sbItem.command = 'extension.mk.allTargets';
+   sbItem.tooltip = 'Permet de lancer une cible du Makefile du projet courant.';
+   context.subscriptions.push( sbItem );
+   console.log( "activate|createStatusBarItem" );
+
+   function makeTarget( dir: string, target: string ) {
+      let term = vscode.window.activeTerminal;
+      if( term === null ) {
+         term = vscode.window.createTerminal( "Make" );
+      }
+      if( term !== undefined ) {
+         term.show( false );
+         term.sendText( "make " + target );
+      }
+   }
+   
+   const targets: string[] = [];
+
+   function parseMakefile( context: vscode.ExtensionContext, path: string ) {
+      require( 'fs' ).readFile( path, ( err: any, data: any ) => {
+         if( err ) { throw err; }
+         const dir   = require( 'path' ).dirname( path );
+         const lines = data.toString().split( '\n' );
+         for( let ndx in lines ) {
+            let line = lines[ndx];
+            let tokens: string[] = line.split( /\s+/ );
+            if(( tokens.length > 1 )&&( tokens[0] === '.PHONY:' )) {
+               sbItem.show();
+               for( let ndxT = 1; ndxT < tokens.length; ++ndxT ) {
+                  const target    = tokens[ndxT];
+                  const commandID = 'extension.mk.' + target;
+                  targets.push( target );
+                  console.log( "parseMakefile|registerCommand '" + commandID + '"' );
+                  context.subscriptions.push(
+                     vscode.commands.registerCommand( commandID, () => makeTarget( dir, target )));
+               }
+            }
+         }
+      });
+   }
+   
+   vscode.workspace
+      .findFiles( '**/Makefile' )
+      .then( v => v.forEach( value => parseMakefile( context, value.path )));
+
+   context.subscriptions.push(
+      vscode.commands.registerCommand(
+         'extension.mk.allTargets',
+         () => vscode.window
+            .showQuickPick( targets, { canPickMany: false })
+            .then( target => {
+               if( target ) {
+                  vscode.commands.executeCommand( 'extension.mk.' + target );
+               }
+            })
+      )
+   );
+   console.log( "activate|registerCommand 'extension.mk.allTargets'" );
+}
+
+export function deactivate() {}
